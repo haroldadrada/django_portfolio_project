d71c4aed7facf13e4033e2cfcff0edcb9de53c46
haroldadrada <andresadrada@gmail.com>
2022-06-11 00:24:47 -0500

    Modificar y crear en (Views, urls, templates, models) de blog y portfolio

--- django_portfolio/blog/models.py
+++ django_portfolio/blog/models.py
@@ -1,3 +1,9 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images')
+    date = models.DateTimeField(datetime.date.today)
--- django_portfolio/blog/views.py
+++ django_portfolio/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def render_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/posts.html', {'posts': posts})
